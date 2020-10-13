c8943525d7233e680155694bf93b75d5b9c2e60f
Jacquita Adams <adamslaquita24@gmail.com>
2020-10-13 18:34:08 -0500

    Initial Commit

---
+++ RandomRestaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomRestaurant
+//
+//  Created by MXC Swift on 10/13/20.
+//  Copyright © 2020 MXC Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
